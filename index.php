5a68cd50cbbef07fb0c5787c59ac238f13819107
sansan06 <purwansyahsansan046@gmail.com>
2024-07-13 20:12:19 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,420 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <title>My Portofolio Sansan</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous" />
+
+  <!-- Bootstrap Icons -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" />
+  <!-- MY CSS  -->
+  <link rel="stylesheet" href="style.css" />
+  <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.min.js" integrity="sha384-heAjqF+bCxXpCWLa6Zhcp4fu20XoNIA98ecBC1YkdXhszjoejr5y9Q77hIrv8R9i" crossorigin="anonymous"></script>
+  <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
+  <script src="https://cdn.datatables.net/1.13.4/js/dataTables.bootstrap5.min.js"></script>
+</head>
+
+<body id="home">
+  <!-- Navbar -->
+  <nav class="navbar navbar-expand-lg navbar-dark fixed-top" style="background-color: #0a6b53">
+    <div class="container">
+      <a class="navbar-brand" href="#">Sansan Purwansyah </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#home">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#About">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#projects">Projects</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <!-- Akhir Navbar -->
+
+  <!-- Jumbotron -->
+  <section class="jumbotron text-center">
+    <img src="img/photo 1.jpg" alt="Sansan Purwansyah" width="200" class="rounded-circle img-thumbnail" />
+    <h1 class="display-4">Sansan Purwansyah</h1>
+    <p class="lead">Journalist & Editing</p>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+      <path fill="#ffffff" fill-opacity="10" d="M0,224L48,208C96,192,192,160,288,176C384,192,480,256,576,250.7C672,245,768,171,864,170.7C960,171,1056,245,1152,250.7C1248,256,1344,192,1392,160L1440,128L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
+    </svg>
+  </section>
+  <!-- Akhir Jumbotron -->
+
+  <!-- About -->
+  <section id="About">
+    <div class="container">
+      <div class="row text-center mb-3">
+        <div class="col">
+          <h2>About Me</h2>
+        </div>
+      </div>
+      <div class="row justify-content-center fs-5 text-center">
+        <div class="col-md-4">
+          <p>
+            Hi everyone! My name is Sansan Purwansyah.Story-driven Journalist adept at developing articles and investigating issues with tenacious and unwavering dedication. Offering 6+ years of experience in print and online media. Versed in viral trends, breaking news response and able to capitalize on social media platforms to get stories out.
+          </p>
+        </div>
+      </div>
+    </div>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+      <path fill="#c8b5b5" fill-opacity="1" d="M0,128L48,128C96,128,192,128,288,154.7C384,181,480,235,576,245.3C672,256,768,224,864,192C960,160,1056,128,1152,117.3C1248,107,1344,117,1392,122.7L1440,128L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
+    </svg>
+  </section>
+  <!-- Akhir About -->
+
+  <!-- Projects -->
+  <section id="projects">
+    <div class="container">
+      <div class="row text-center mb-3">
+        <div class="col">
+          <h2>My Projects</h2>
+        </div>
+      </div>
+      <div class="row justify-content-center text-center">
+        <div class="col-md-4 mb-3">
+          <div class="card">
+            <img src="img/projects/photo 1.jpg" class="card-img-top" alt="projects 1" />
+            <div class="card-body">
+              <p class="card-text">Pengumumam Hasil Pemilu 2024</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 mb-3">
+          <div class="card">
+            <img src="img/projects/photo 2.jpg" class="card-img-top" alt="projects 2" />
+            <div class="card-body">
+              <p class="card-text">Musuh Baru Israel</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 mb-3">
+          <div class="card">
+            <img src="img/projects/photo 3.jpg" class="card-img-top" alt="projects 3" />
+            <div class="card-body">
+              <p class="card-text">Demo Buruh 1 Mei</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+      <path fill="#ffffff" fill-opacity="1" d="M0,96L48,85.3C96,75,192,53,288,85.3C384,117,480,203,576,218.7C672,235,768,181,864,176C960,171,1056,213,1152,213.3C1248,213,1344,171,1392,149.3L1440,128L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
+    </svg>
+  </section>
+
+  <!-- Akhir Projects -->
+
+  <!-- Contact -->
+  <section id="contact">
+    <div class="container">
+      <div class="row text-center mb-3">
+        <div class="col">
+          <h2>Contact Me</h2>
+        </div>
+      </div>
+      <div class="row justify-content-center">
+        <div class="col-md-6">
+          <form>
+            <div class="mb-3">
+              <label for="Name" class="form-label">Nama Lengkap</label>
+              <input type="text" class="form-control" id="name" aria-describedby="name" />
+            </div>
+            <div class="mb-3">
+              <label for="email" class="form-label">Email</label>
+              <input type="email" class="form-control" id="email" aria-describedby="email" />
+            </div>
+            <div class="mb-3">
+              <label for="Pesan" class="form-label">Pesan</label>
+              <textarea class="form-control" id="Pesan" rows="3"></textarea>
+            </div>
+            <button type="kirim" class="btn btn-primary">Kirim</button>
+          </form>
+        </div>
+      </div>
+    </div>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+      <path fill="#c8b5b5" fill-opacity="1" d="M0,96L48,85.3C96,75,192,53,288,80C384,107,480,181,576,197.3C672,213,768,171,864,170.7C960,171,1056,213,1152,229.3C1248,245,1344,235,1392,229.3L1440,224L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
+    </svg>
+  </section>
+  <!-- Akhir Contact -->
+  <!-- User Skills -->
+
+  <section id="skills">
+    <div class="container">
+      <div id="message"></div>
+
+      <h1 class="mt-4 mb-4 text-center text-danger">SKILLS USER CRUD</h1>
+      <div class="card">
+        <div class="card-header">
+          <div class="row">
+            <div class="col col-sm-9">SKILLS</div>
+            <div class="col col-sm-3">
+              <button type="button" id="add_data" class="btn btn-success btn-sm float-end">Add</button>
+            </div>
+          </div>
+        </div>
+        <div class="card-body">
+          <div class="table-responsive">
+            <table class="table table-striped table-bordered" id="sample_data">
+              <thead>
+                <tr>
+                  <th>User ID</th>
+                  <th>Skill Name</th>
+                  <th>Rating</th>
+                  <th>Description</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody></tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+      <div class="modal" tabindex="-1" id="action_modal">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <form method="post" id="sample_form">
+              <div class="modal-header">
+                <h5 class="modal-title" id="dynamic_modal_title"></h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div class="modal-body">
+                <div class="mb-3">
+                  <label class="form-label">User ID</label>
+                  <input type="text" name="user_id" id="user_id" class="form-control" />
+                  <span id="user_id_error" class="text-danger"></span>
+                </div>
+                <div class="mb-3">
+                  <label class="form-label">Skill Name</label>
+                  <input type="skill_name" name="skill_name" id="skill_name" class="form-control" />
+                  <span id="skill_name_error" class="text-danger"></span>
+                </div>
+                <div class="mb-3">
+                  <label class="form-label">Rating</label>
+                  <input type="rating" name="rating" id="rating" class="form-control" />
+                  <span id="rating_error" class="text-danger"></span>
+                </div>
+                <div class="mb-3">
+                  <label class="form-label">Description</label>
+                  <input type="text" name="description" id="description" class="form-control" />
+                  <span id="description_error" class="text-danger"></span>
+                </div>
+              </div>
+              <div class="modal-footer">
+                <input type="hidden" name="id" id="id" />
+                <input type="hidden" name="action" id="action" value="Add" />
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="submit" class="btn btn-primary" id="action_button">Add</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+  <!-- Footer -->
+  <footer class="bg-color=#f7c9c9 text-black text-center pb-5">
+    <p>Created with <i class="bi bi-instagram"></i></i> by <a href="https://www.instagram.com/sansanprwnsyh_?igsh=d3BtMzlhaDVudGo2" class="text-black">Sansan Purwansyah</a></p>
+  </footer>
+  <!-- Akhir Footer -->
+  <script>
+    $(document).ready(function() {
+      showAll();
+
+      function showAll() {
+        $.ajax({
+          type: "GET",
+          contentType: "application/json",
+          url: "http://localhost/web-portofolio-sansan/si-admin/api/users/read.php?id=",
+          success: function(response) {
+            $("#full_name").text(response.full_name); //mengubah elemen html dengan .text(nilai) atau .html(nilai)
+            $("#job_position").text(response.job + " | " + response.expected_position);
+          },
+          error: function(err) {},
+        });
+      }
+    });
+  </script>
+
+  <script>
+    $(document).ready(function() {
+      showAll();
+
+      $("#add_data").click(function() {
+        $("#dynamic_modal_title").text("Add User Skills");
+        $("#sample_form")[0].reset();
+        $("#action").val("Add");
+        $("#action_button").text("Add");
+        $(".text-danger").text("");
+        $("#action_modal").modal("show");
+      });
+
+      $("#sample_form").on("submit", function(event) {
+        event.preventDefault();
+        if ($("#action").val() == "Add") {
+          var formData = {
+            user_id: $("#user_id").val(),
+            skill_name: $("#skill_name").val(),
+            rating: $("#rating").val(),
+            description: $("#description").val(),
+          };
+
+          $.ajax({
+            url: "http://localhost/portofolio-sansan/si-admin/api/skills/create.php",
+            method: "POST",
+            data: JSON.stringify(formData),
+            success: function(data) {
+              $("#action_button").attr("disabled", false);
+              $("#message").html('<div class="alert alert-success">' + data.message + "</div>");
+              $("#action_modal").modal("hide");
+              $("#sample_data").DataTable().destroy();
+              showAll();
+            },
+            error: function(err) {
+              console.log(err);
+            },
+          });
+        } else if ($("#action").val() == "Update") {
+          var formData = {
+            id: $("#id").val(),
+            user_id: $("#user_id").val(),
+            skill_name: $("#skill_name").val(),
+            rating: $("#rating").val(),
+            description: $("#description").val(),
+          };
+
+          $.ajax({
+            url: "http://localhost/portofolio-sansan/si-admin/api/skills/update.php",
+            method: "PUT",
+            data: JSON.stringify(formData),
+            success: function(data) {
+              $("#action_button").attr("disabled", false);
+              $("#message").html('<div class="alert alert-success">' + data.message + "</div>");
+              $("#action_modal").modal("hide");
+              $("#sample_data").DataTable().destroy();
+              showAll();
+            },
+            error: function(err) {
+              console.log(err);
+            },
+          });
+        }
+      });
+    });
+
+    function showAll() {
+      $.ajax({
+        type: "GET",
+        contentType: "application/json",
+        url: "http://localhost/portofolio-sansan/si-admin/api/skills/read.php",
+        success: function(response) {
+          // console.log(response);
+          var json = response.body;
+          var dataSet = [];
+          for (var i = 0; i < json.length; i++) {
+            var sub_array = {
+              user_id: json[i].user_id,
+              skill_name: json[i].skill_name,
+              rating: json[i].rating,
+              description: json[i].description,
+              action: '<button onclick="showOne(' + json[i].id + ')" class="btn btn-sm btn-warning">Edit</button>' + '<button onclick="deleteOne(' + json[i].id + ')" class="btn btn-sm btn-danger">Delete</button>',
+            };
+            dataSet.push(sub_array);
+          }
+          $("#sample_data").DataTable({
+            data: dataSet,
+            columns: [{
+                data: "user_id",
+              },
+              {
+                data: "skill_name",
+              },
+              {
+                data: "rating",
+              },
+              {
+                data: "description",
+              },
+              {
+                data: "action",
+              },
+            ],
+          });
+        },
+        error: function(err) {
+          console.log(err);
+        },
+      });
+    }
+
+    function showOne(id) {
+      $("#dynamic_modal_title").text("Edit User Skills");
+      $("#sample_form")[0].reset();
+      $("#action").val("Update");
+      $("#action_button").text("Update");
+      $(".text-danger").text("");
+      $("#action_modal").modal("show");
+
+      $.ajax({
+        type: "GET",
+        contentType: "application/json",
+        url: "http://localhost/portofolio-sansan/si-admin/api/skills/read.php?id=" + id,
+        success: function(response) {
+          $("#id").val(response.id);
+          $("#user_id").val(response.user_id);
+          $("#skill_name").val(response.skill_name);
+          $("#rating").val(response.rating);
+          $("#description").val(response.description);
+        },
+        error: function(err) {
+          console.log(err);
+        },
+      });
+    }
+
+    function deleteOne(id) {
+      alert("Yakin untuk hapus data ?");
+      $.ajax({
+        url: "http://localhost/portofolio-sansan/si-admin/api/skills/delete.php",
+        method: "DELETE",
+        data: JSON.stringify({
+          id: id,
+        }),
+        success: function(data) {
+          $("#action_button").attr("disabled", false);
+          $("#message").html('<div class="alert alert-success">' + data + "</div>");
+          $("#action_modal").modal("hide");
+          $("#sample_data").DataTable().destroy();
+          showAll();
+        },
+        error: function(err) {
+          console.log(err);
+        },
+      });
+    }
+  </script>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+   
+</body>
+
+</html>
